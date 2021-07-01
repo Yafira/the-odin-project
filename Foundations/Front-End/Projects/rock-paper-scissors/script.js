30f8f9724e42874fd630ea9d1a7b04bbc62b8488
Yafira <yafira.martinez@lc.cuny.edu>
2021-07-01 17:50:07 -0400

    wrote function that allows for a single round play

--- Foundations/Front-End/Projects/rock-paper-scissors/script.js
+++ Foundations/Front-End/Projects/rock-paper-scissors/script.js
@@ -1,1 +1,51 @@
-console.log("Hello World");
+// randomly return either 'Rock', 'Paper', 'Scissors'
+
+let items = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay() {
+    let result = items[Math.floor(Math.random() * items.length)];
+    return result;
+}
+
+// plays a single round of Rock Paper Scissors, return string
+function playRound(playerSelection, computerSelection) {
+    let playerSelection = prompt("Choose: Rock/Paper/Scissors",'');
+    let computerSelection = computerPlay();
+
+    if (playerSelection.toLowerCase() == "Rock".toLowerCase()){
+        if (computerSelection === "Scissors"){
+            ++playerScore;
+            return "You win! Rock beats Scissors.";
+        } else if(computerSelection === "Paper"){
+            ++computerScore;
+            return "You lose! Paper beats Rock.";
+        } else {
+            return "Tie!";
+            }
+        } else if (playerSelection.toLowerCase() == "Paper".toLowerCase()){
+            if (computerSelection === "Rock"){
+                ++playerScore;
+                return "You win! Paper beats Rock.";
+            } else if(computerSelection === "Scissors"){
+                ++computerScore;
+                return "You lose! Scissors beats Paper.";
+            } else {
+                return "Tie!";
+            }
+        } else if (playerSelection.toLowerCase() == "Scissors".toLowerCase()) {
+            if (computerSelection === "Paper"){
+                ++playerScore;
+                return "You win! Scissors beats Paper.";
+            } else if (computerSelection === "Rock"){
+                ++computerScore;
+                return "You lose! Rock beats Scissors.";
+            } else {
+                return "Tie!"
+            }
+        }
+
+        const playerSelection = "rock";
+        const computerSelection = computerPlay();
+        //return results
+        console.log(playRound(playerSelection, computerSelection));
+}
