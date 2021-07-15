2cfee5e23cfe561e7381c053df32c761efd5740b
Yafira <yafira.martinez@lc.cuny.edu>
2021-07-14 21:09:33 -0400

    added more elements to the container

--- Foundations/JavaScript Basics/dom-manipulation.js
+++ Foundations/JavaScript Basics/dom-manipulation.js
@@ -8,3 +8,17 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 // append the div to the container
 container.appendChild(content);
+
+// create new paragraph under content
+const paragraph = document.createElement('p');
+paragraph.textContent = "Hey I\'m RED!"
+paragraph.style.color = 'red';
+content.appendChild(paragraph);
+
+// create a header size 3 under content
+const header3 = document.createElement('h3');
+header3.textContent = "I\'m a blue h3!";
+header3.style.color = 'blue';
+content.appendChild(header3);
+
+
