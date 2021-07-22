4a1c82b55183f49fa5d3b74e4c2aa4305ff259a5
Yafira <yafira.martinez@lc.cuny.edu>
2021-07-22 18:23:05 -0400

    completed score functionality

--- Foundations/Front-End/Projects/rock-paper-scissors/script.js
+++ Foundations/Front-End/Projects/rock-paper-scissors/script.js
@@ -11,13 +11,19 @@ let computerScore = 0;
         const computerSelection = compOptions[Math.floor(Math.random() * 3)];
 
         playRound(playerSelection, computerSelection);
+        updateScore();
+
+        if (checkWinner()) {
+            playerScore = computerScore = 0;
+            updateScore();
+        }
     });
 });
 
 function playRound(playerSelection, computerSelection) {
     const currentMatch = `${playerSelection} vs ${computerSelection}`;
 
-    // Check if it's a tie
+    // check if it's a tie
     if (playerSelection === computerSelection) {
         alert(`${currentMatch} is a Tie!`);
         return;
@@ -27,8 +33,10 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock") {
         if (computerSelection === "Scissors") {
             alert(`${currentMatch} = You Win!`);
+            playerScore++;
         } else {
             alert(`${currentMatch} = Computer Wins!`);
+            computerScore++;
         }
     }
 
@@ -36,16 +44,38 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
             alert(`${currentMatch} = You Win!`);
+            playerScore++;
         } else {
             alert(`${currentMatch} = Computer Wins!`);
+            computerScore++;
         }
     }
     // Scissors
     else if (playerSelection === "Scissors") {
         if (computerSelection === "Paper") {
             alert(`${currentMatch} = You Win!`);
+            playerScore++;
         } else {
             alert(`${currentMatch} = Computer Wins!`);
+            computerScore++;
         }
     }
+}
+// update score after each round
+function updateScore() {
+    document.getElementById("p-score").textContent = playerScore;
+    document.getElementById("c-score").textContent = computerScore;
+}
+
+// check who's the winner after 5 rounds
+function checkWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+        const winner =
+        playerScore === 5
+            ? "You win the game! Congratulations!"
+            : "Computer wins the game! Try again next time!";
+        alert(winner);
+        return true;
+    }
+    return false;
 }
