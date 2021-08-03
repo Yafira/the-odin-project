7d40b87ef2fc873047a8ff192842a84e21dda1f3
Yafira <yafira.martinez@lc.cuny.edu>
2021-08-02 20:21:14 -0400

    implementing hover effect

--- Foundations/Front-End/Projects/etch-a-sketch/script.js
+++ Foundations/Front-End/Projects/etch-a-sketch/script.js
@@ -1,15 +1,20 @@
 // 16x16 grid of square divs
-function createGrid(x, y, cellSize, gridElement) {
+function createGrid(rows, cols, cellSize, gridElement) {
     gridElement.style.display = "grid";
-    gridElement.style.gridTemplateColumns = `repeat(${x}, ${cellSize}px)`;
-    gridElement.style.gridTemplateRows = `repeat(${y}, ${cellSize}px)`;
+    gridElement.style.gridTemplateColumns = `repeat(${cols}, ${cellSize}px)`;
+    gridElement.style.gridTemplateRows = `repeat(${rows}, ${cellSize}px)`;
 
     let squares = new DocumentFragment();
 
-    for (let i = 0; i < x * y; i++) {
+    for (let i = 0; i < rows * cols; i++) {
         let square = document.createElement('div');
         square.className = 'square';
         squares.appendChild(square);
+
+        // hover effect
+        gridElement.addEventListener('mouseover', e =>
+        e.target.classList.add('square-color')
+)
     }
 
     gridElement.appendChild(squares);
