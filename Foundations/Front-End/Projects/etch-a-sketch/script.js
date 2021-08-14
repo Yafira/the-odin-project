3e201eb530afead2392b68ed9d36ac2e345a86eb
Yafira <yafira.martinez@lc.cuny.edu>
2021-08-14 01:31:08 -0400

    changed cell color, added footer + other revisions

--- Foundations/Front-End/Projects/etch-a-sketch/script.js
+++ Foundations/Front-End/Projects/etch-a-sketch/script.js
@@ -1,43 +1,42 @@
-const grid = document.querySelector(".container");
+const grid = document.querySelector(".canvas");
 const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset");
 
 // create 16x16 grid of 256 square divs
-createGrid = () => {
+createCanvas = () => {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement("div");
-        div.classList.add("square");
+        div.classList.add("cell");
         grid.appendChild(div);
     }
 };
 
 // user can choose value of grid size
-updateGrid = () => {
+updateCanvas = () => {
     grid.innerHTML = "";
     grid.style.setProperty(
         "grid-template-columns",
         `repeat(${userInput.value}, 2fr)`
     );
-
     grid.style.setProperty("grid-template-rows",
     `repeat(${userInput.value}, 2fr)`);
 
     for (let i = 0; i < userInput.value * userInput.value; i++) {
         const div = document.createElement("div");
-        div.classList.add("square");
+        div.classList.add("cell");
         grid.appendChild(div);
     }
     console.log(userInput.value);
 };
 
 // hover effect
-const square = document.querySelector("div");
-square.addEventListener("mouseover", function(event) {
-    event.target.classList.replace("square", "color");
+const cell = document.querySelector("div");
+cell.addEventListener("mouseover", function(event) {
+    event.target.classList.replace("cell", "color");
 });
 
 // update grid with user input
-userInput.addEventListener("change", updateGrid);
+userInput.addEventListener("change", updateCanvas);
 
 // resets grid to default values
 resetButton.addEventListener("click", function() {
@@ -45,7 +44,7 @@ resetButton.addEventListener("click", function() {
     userInput.value = "";
     grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
     grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-    createGrid();
+    createCanvas();
 });
 
-createGrid();
+createCanvas();
