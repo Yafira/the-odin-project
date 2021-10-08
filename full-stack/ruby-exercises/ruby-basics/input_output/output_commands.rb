d57acdb1ab5501c94acac6298d04574a9b7c3aba
Yafira <yafira.martinez@lc.cuny.edu>
2021-10-08 19:24:43 -0400

    learning about output commands

---
+++ full-stack/ruby-exercises/ruby-basics/input_output/output_commands.rb
@@ -0,0 +1,34 @@
+# print command
+# print keeps things all on one line
+print "Learning to code is FUN!"
+# "Learning to code is FUN!" => nil
+
+print "1234"
+# 1234 => nil
+
+# puts command
+# puts appends a new line to the argument passed in
+puts "Learning to code is cool!!"
+# Learning to code is cool!!
+# => nil
+
+puts "Hey, I want 2 key lime pies."
+# Hey, I want 2 key lime pies.
+# => nil
+
+x = "My name is ALEX! :)"
+# "My name is ALEX! :)"
+
+puts x
+# My name is ALEX! :)
+# => nil
+
+
+# Highlighting the difference between puts and print with semicolon
+print "Hello World"; print "I love drinking coffee"
+# Hello WorldI love drinking coffee=> nil
+
+puts "Hello World"; puts "I love drinking coffee"
+# Hello World
+# I love drinking coffee
+# => nil
