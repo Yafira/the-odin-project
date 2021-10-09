07c042c17ef7f2ce84b38f3efb0e98693b45f082
Yafira <yafira.martinez@lc.cuny.edu>
2021-10-08 22:11:43 -0400

    added  more examples to i/o commands

--- full-stack/ruby-exercises/ruby-basics/input_output/input_commands.rb
+++ full-stack/ruby-exercises/ruby-basics/input_output/input_commands.rb
@@ -2,26 +2,40 @@
 # When we use gets, program execution will stop and wait for user input. After the user presses Enter, the program will continue its execution.
 gets
 The Odin Project
-=> "The Odin Project\n"  # unlike puts and prints, gets returns the user input instead of returning nil
+# => "The Odin Project\n"  # unlike puts and prints, gets returns the user input instead of returning nil
+
+puts "Enter a value :"
+val = gets
+puts val
+# Enter a value :
+# This is entered value
+# This is entered value
 
 new_string = gets.chomp # chomp is a method commonly used to trim separators
 This is a sentence.
-=> "This is a sentence."
+# => "This is a sentence."
 
 puts new_string
 This is a sentence.
-=> nil
+# => nil
 
 puts "I will be printed to the console!"
 
-p 'Hello world' # p is a shorter form of puts
-"Hello world"
-=> "Hello world"
-
-
 puts 'Hello World !!!'
 Hello World !!!
-=> nil
+# => nil
 
 print 'Hello World !!!'
-Hello World !!!=> nil
+# Hello World !!!=> nil
+
+p 'Hello world' # p is a shorter form of puts
+"Hello world"
+# => "Hello world"
+
+val1 = "This is variable one"
+val2 = "This is variable two"
+puts val1
+puts val2
+# This is variable one
+# This is variable two
+
--- full-stack/ruby-exercises/ruby-basics/input_output/output_commands.rb
+++ full-stack/ruby-exercises/ruby-basics/input_output/output_commands.rb
@@ -4,31 +4,40 @@ print "Learning to code is FUN!"
 # "Learning to code is FUN!" => nil
 
 print "1234"
-1234 => nil
+# 1234 => nil
+
+print "Hello World"
+print "Good Morning"
+# Hello WorldGood Morning
 
 # Puts command
 # puts appends a new line to the argument passed in
 puts "Learning to code is cool!!"
-Learning to code is cool!!
-=> nil
+# Learning to code is cool!!
+# => nil
 
 puts "Hey, I want 2 key lime pies."
-Hey, I want 2 key lime pies.
-=> nil
+# Hey, I want 2 key lime pies.
+# => nil
 
 x = "My name is ALEX! :)"
 # "My name is ALEX! :)"
 
 puts x
-My name is ALEX! :)
-=> nil
-
+# My name is ALEX! :)
+# => nil
 
 # Highlighting the difference between puts and print with semicolon
 print "Hello World"; print "I love drinking coffee"
-Hello WorldI love drinking coffee=> nil
+# Hello WorldI love drinking coffee=> nil
 
 puts "Hello World"; puts "I love drinking coffee"
-Hello World
-I love drinking coffee
-=> nil
+# Hello World
+# I love drinking coffee
+# => nil
+
+# Putsc command
+# Unlike puts, which outputs the entire string onto the screen, the putc statement can be used to output one character at a time.
+str = "Hello Ruby!"
+putc str
+# H
