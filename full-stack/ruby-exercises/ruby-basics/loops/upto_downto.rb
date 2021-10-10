170a7c306168732bee26d1166512725aa74d11d4
Yafira <yafira.martinez@lc.cuny.edu>
2021-10-10 14:59:15 -0400

    learining about upto and downto loops

---
+++ full-stack/ruby-exercises/ruby-basics/loops/upto_downto.rb
@@ -0,0 +1,8 @@
+# Upto and Downto loop
+# iterate from a starting number, either up to or down to another number
+
+# Upto loop
+5.upto(10) {|num| print "#{num} " }     #=> 5 6 7 8 9 10
+
+# Downto loop
+10.downto(5) {|num| print "#{num} " }   #=> 10 9 8 7 6 5
