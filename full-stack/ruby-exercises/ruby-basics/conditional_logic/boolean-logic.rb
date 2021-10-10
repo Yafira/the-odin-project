15a0aa7166e50621de977c5b545ca0e134ea0b6b
Yafira <yafira.martinez@lc.cuny.edu>
2021-10-09 23:40:19 -0400

    reviewing boolean logic

---
+++ full-stack/ruby-exercises/ruby-basics/conditional_logic/boolean-logic.rb
@@ -0,0 +1,45 @@
+# Boolean logic, true or false
+
+# == (equals) returns true if the values compared are equal
+5 == 5 #=> true
+5 == 6 #=> false
+
+# != (not equal) returns true if the values compared are not equal.
+5 != 7 #=> true
+5 != 5 #=> false
+
+# > (greater than) returns true if the value on the left of the operator is larger than the value on the right.
+7 > 5 #=> true
+5 > 7 #=> false
+
+# < (less than) returns true if the value on the left of the operator is smaller than the value on the right
+5 < 7 #=> true
+7 < 5 #=> false
+
+# >= (greater than or equal to) returns true if the value on the left of the operator is larger than or equal to the value on the right.
+7 >= 7 #=> true
+7 >= 5 #=> true
+
+# <= (less than or equal to) returns true if the value on the left of the operator is smaller than or equal to the value on the right
+5 <= 5 #=> true
+5 <= 7 #=> true
+
+#eql? checks both the value type and the actual value it holds
+5.eql?(5.0) #=> false; although they are the same value, one is an integer and the other is a float
+5.eql?(5)   #=> true
+
+#equal? checks whether both values are the exact same object in memory.
+# Two variables pointing to the same number will usually return true.
+a = 5
+b = 5
+a.equal?(b) #=> true
+
+
+## Spaceship operator (<=>)
+# returns one of three numerical values
+# -1 if the value on the left is less than the value on the right;
+# 0 if the value on the left is equal to the value on the right; and
+# 1 if the value on the left is greater than the value on the right.
+5 <=> 10    #=> -1
+10 <=> 10   #=> 0
+10 <=> 5    #=> 1
