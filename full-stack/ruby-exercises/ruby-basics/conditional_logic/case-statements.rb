bc595cc7f00ff6370f8a384147652bf4c6f273e2
Yafira <yafira.martinez@lc.cuny.edu>
2021-10-10 00:30:33 -0400

    added more examples to case statements

--- full-stack/ruby-exercises/ruby-basics/conditional_logic/case-statements.rb
+++ full-stack/ruby-exercises/ruby-basics/conditional_logic/case-statements.rb
@@ -22,3 +22,53 @@ else
     puts "'YOU SHALL NOT PASS!' -Gandalf"
     fml = true
 end
+
+# more examples to case statements
+a = 5
+
+case a
+when 5
+    puts "a is 5"
+when 6
+    puts "a is 6"
+else
+    puts "a is neither 5, nor 6"
+end
+
+# same as this if statement:
+
+a = 5
+
+if a == 5
+    puts "a is 5"
+elsif a == 6
+    puts "a is 6"
+else
+    puts "a is neither 5, nor 6"
+end
+
+# refactored version
+a = 5
+
+answer = case a
+when 5
+    "a is 5"
+when 6
+    "a is 6"
+else
+    "a is neither 5, nor 6"
+end
+
+puts answer
+
+# another form without an argument
+a = 5
+
+case
+when a == 5
+    puts "a is 5"
+when a == 6
+    puts "a is 6"
+else
+    puts "a is neither 5, nor 6"
+end
