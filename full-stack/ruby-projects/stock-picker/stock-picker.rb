3d8139a4b98beee56bb76d9c082ad69dd8d2d487
Yafira <yafira.martinez@lc.cuny.edu>
2021-10-21 17:50:31 -0400

    completed Stock Picker project

--- full-stack/ruby-projects/stock-picker/stock-picker.rb
+++ full-stack/ruby-projects/stock-picker/stock-picker.rb
@@ -1,1 +1,19 @@
 # Stock Picker
+
+def stock_picker(prices)
+    profit = 0
+    days = 0
+
+    prices.each_with_index do |price, day|
+        for i in day..prices.length - 1
+        if prices[i] - price > profit
+            profit = prices[i] - price
+            days = [day, i]
+            end
+        end
+    end
+    days
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
+# [1,4]  # for a profit of $15 - $3 == $12
