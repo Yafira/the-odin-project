88a8e0ff6629a920876272041ba80ce4c8a25817
Yafira <yafira.martinez@lc.cuny.edu>
2022-03-09 18:21:45 -0500

    added pgs text and changed font size

--- full-stack/javascript/library/script.js
+++ full-stack/javascript/library/script.js
@@ -16,7 +16,7 @@ class Book {
 	constructor(title, author, pages, read) {
 		this.title = form.title.value;
 		this.author = form.author.value;
-		this.pages = form.pages.value;
+		this.pages = form.pages.value + ' page(s)';
 		this.read = form.read.checked;
 	}
 }
